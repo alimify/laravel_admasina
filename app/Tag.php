87142bbf5c85b0ca79e32a4d925d172277ccea35
Abdul Alim <jeweleuro@gmail.com>
2018-09-01 17:42:59 +0600

    Tag model fixes

--- app/Tag.php
+++ app/Tag.php
@@ -9,11 +9,11 @@ class Tag extends Model
 
 
     public function books(){
-        return $this->belongsToMany('App\book');
+        return $this->belongsToMany('App\Book');
     }
 
     public function posts(){
-        return $this->belongsToMany('App\post');
+        return $this->belongsToMany('App\Post');
     }
 
 }
