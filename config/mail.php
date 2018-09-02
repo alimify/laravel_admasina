3c47177989157955eff156887ce39dd330701339
Abdul Alim <jeweleuro@gmail.com>
2018-09-02 10:37:42 +0600

    Mail Fixing

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'admasina.com'),
+    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
 
     /*
     |--------------------------------------------------------------------------
